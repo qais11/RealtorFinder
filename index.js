38d2ecab8d2c85f386649b761eb6da819d88a487
Samer <samaladhami@gmail.com>
2018-12-07 19:51:27 -0600

    pushing to heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const emailSender = require('./.config.js');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/dist/')))
 
