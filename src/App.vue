40f2d0d3fe20a0b5f136cb9333f57a2563cba3f3
Samer <samaladhami@gmail.com>
2018-12-08 18:17:51 -0600

    header fixing

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <Map/>
-    <side-bar v-on:openSideBar="openSideBar()"></side-bar>
     <Nav/>
+    <side-bar v-on:openSideBar="openSideBar()"></side-bar>
+    <Map/>
     <div ref="popUp"> 
       <Info/>
     </div>
@@ -59,7 +59,7 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
   position: relative;
 }
 
