a1a0b8f028219527dd421604873101d70451e78c
= <qais.gh11@gmail.com>
2019-02-17 20:58:24 -0600

    added company url

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        Site design / logo / code © 2018 <a href="88techs.net">Eighty Eight Tech</a>
+        Site design / logo / code © 2018 <a href="https://88techs.net" target="_blank">Eighty Eight Tech</a>
     </footer>
 </template>
 <script src="./Footer.js"></script>
