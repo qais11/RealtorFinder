9a66347ee62c4b220003d9b9d8fda9824b369c92
= <qais.gh11@gmail.com>
2019-02-17 20:52:51 -0600

    added company url

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        Site design / logo / code © 2018 88Tech 
+        Site design / logo / code © 2018 <a href="88techs.net">Eighty Eight Tech</a>
     </footer>
 </template>
 <script src="./Footer.js"></script>
